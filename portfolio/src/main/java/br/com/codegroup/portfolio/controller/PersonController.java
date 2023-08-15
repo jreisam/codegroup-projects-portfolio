d80c95c1f3c763be7176e88586b8301b78c8c98c
João Robson <jreisam@gmail.com>
2023-08-14 21:14:06 -0400

    Cruds Project, Person: OK.

---
+++ portfolio/src/main/java/br/com/codegroup/portfolio/controller/PersonController.java
@@ -0,0 +1,61 @@
+package br.com.codegroup.portfolio.controller;
+
+import br.com.codegroup.portfolio.model.Person;
+import br.com.codegroup.portfolio.service.PersonService;
+import br.com.codegroup.portfolio.service.PersonService;
+import br.com.codegroup.portfolio.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+/**
+ * @author João Robson 14/08/2023
+ */
+
+@RestController
+@RequestMapping("/person")
+@CrossOrigin(origins = "*")
+public class PersonController {
+    @Autowired
+    private ProjectService projectService;
+
+    @Autowired
+    private PersonService personService;
+
+    @PostMapping
+    public ResponseEntity<Person> createPerson(@Valid @RequestBody Person person) {
+        Person savedPerson = personService.createPerson(person);
+        return new ResponseEntity<>(savedPerson, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Person> updatePerson(@PathVariable("id") Long personId,
+                                                 @Valid @RequestBody Person person) {
+        person.setId(personId);
+        Person updatedPerson = personService.updatePerson(person);
+        return new ResponseEntity<>(updatedPerson, HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deletePerson(@PathVariable("id") Long personId) {
+        personService.deletePerson(personId);
+        return new ResponseEntity<>("Person successfully deleted!", HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Person> getPersonById(@PathVariable("id") Long personId) {
+        Person person = personService.getPersonById(personId);
+        return new ResponseEntity<>(person, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Person>> getAllPersons() {
+        List<Person> persons = personService.getAllPersons();
+        return new ResponseEntity<>(persons, HttpStatus.OK);
+    }
+
+}
--- portfolio/src/main/java/br/com/codegroup/portfolio/service/ProjectService.java
+++ portfolio/src/main/java/br/com/codegroup/portfolio/service/ProjectService.java
@@ -6,39 +6,19 @@ import br.com.codegroup.portfolio.repository.ProjectRepository;
 import org.apache.kafka.common.errors.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author João Robson 12/08/2023
  */
-public class ProjectService {
+public interface ProjectService {
 
-
-    @Autowired
-    private ProjectRepository projectRepository;
-    @Autowired
-    private PersonRepository personRepository;
-
-    public List<Project> findAll() {
-        return projectRepository.findAll();
-    }
-
-    public Project findById(final Long id) {
-        return projectRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException(this.getClass().getName() + " not found!"));
-    }
-
-
-    public void save(final Project project) {
-        projectRepository.save(project);
-    }
-
-    public void update(Long id, Project project) {
-        Project projectOld = findById(id);
-        project.setId(projectOld.getId());
-
-        projectRepository.save(project);
-    }
-
+    Project createProject(Project project);
+    Project getProjectById(Long projectId);
+    List<Project> getAllProjects();
+    Project updateProject(Project project);
+    void deleteProject(Long projectId);
 
 }
